b7416694f0c97101c8a8b933bb61943b0cd8e854
kikunoblog <vikutowaarupisa@gmail.com>
2021-12-08 22:12:36 +0900

    7

--- js/script.js
+++ js/script.js
@@ -3,14 +3,15 @@
     // 横からスライド
     if (window.matchMedia('(max-width: 1024px) and (min-width:320px)').matches) {
 		/* ウィンドウサイズ320以上、以下の処理を記述 */
-    $("#p-header__menu").click(function(){
-      $(this ).toggleClass("open");
-      $(".p-sidebar ").toggleClass("open");
+    $(".p-header__menu").click(function(){
+      $(".p-sidebar ").toggleClass("is-open");
+      $(".l-container").toggleClass("is-open2");
     });
 
-      $("#p-menu-trigger").click(function(){
+      $(".p-menu-trigger").click(function(){
     //openクラスの要素を削除
-    $(".open").remove();
+    $(".is-open").remove();
+    $(".is-open2").remove();
   
     });
   }});

a3d13438817478938079b2709a63139c4e6d9fe4
kikunoblog <vikutowaarupisa@gmail.com>
2021-12-13 12:41:31 +0900

    13

--- js/script.js
+++ js/script.js
@@ -1,19 +1,15 @@
+// 横からスライド
+//if (window.matchMedia('(max-width: 1024px) and (min-width:320px)').matches) {
+/* ウィンドウサイズ320以上、以下の処理を記述 */
+$(function () {
+  $(".p-header__menu").on("click", function () {
+    $(".p-sidebar ").toggleClass("is-open");
+    $(".l-container").toggleClass("is-open2");
+  });
 
-  $(function(){
-    // 横からスライド
-    if (window.matchMedia('(max-width: 1024px) and (min-width:320px)').matches) {
-		/* ウィンドウサイズ320以上、以下の処理を記述 */
-    $(".p-header__menu").click(function(){
-      $(".p-sidebar ").toggleClass("is-open");
-      $(".l-container").toggleClass("is-open2");
-    });
-
-      $(".p-menu-trigger").click(function(){
+  $(".p-menu-trigger").on("click", (function () {
     //openクラスの要素を削除
-    $(".is-open").remove();
-    $(".is-open2").remove();
-  
-    });
-  }});
-
- 
+    $(".p-sidebar").removeClass("is-open");
+    $(".l-container").removeClass("is-open2");
+  }));
+});

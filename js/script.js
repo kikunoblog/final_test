af08b1b8c5a5d102deb795c185cfb574bc898c32
kikunoblog <vikutowaarupisa@gmail.com>
2022-02-07 23:51:58 +0900

    7

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@
 //if (window.matchMedia('(max-width: 1024px) and (min-width:320px)').matches) {
 /* ウィンドウサイズ320以上、以下の処理を記述 */
 $(function () {
-  $(".p-header__menu").on("click", function () {
-    $(".p-sidebar ").toggleClass("is-open");
-    $(".l-container").toggleClass("is-open2");
+  $(".js-menu-button").on("click", function () {
+    $(".p-sidebar ").toggleClass("is-open p-sidebar_move");
+    $(".l-container").toggleClass("is-open cover");
     $("body").toggleClass("fixed");
   });
 
-  $(".p-menu-trigger").on("click", function () {
+  $(".js-menu-close-button").on("click", function () {
     //openクラスの要素を削除
-    $(".p-sidebar").removeClass("is-open");
-    $(".l-container").removeClass("is-open2");
+    $(".p-sidebar").removeClass("is-open p-sidebar_move");
+    $(".l-container").removeClass("is-open cover");
     $("body").toggleClass("fixed");
   });
 });

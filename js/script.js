df8c44c6789fccc9fea7cac91b35a7e94ea8eaa8
kikunoblog <vikutowaarupisa@gmail.com>
2022-02-19 22:19:01 +0900

    JSで設定したクラスには最低限の挙動のみにして、背景を変化させるレイアウトを設定させる

--- js/script.js
+++ js/script.js
@@ -2,16 +2,19 @@
 //if (window.matchMedia('(max-width: 1024px) and (min-width:320px)').matches) {
 /* ウィンドウサイズ320以上、以下の処理を記述 */
 $(function () {
+  /*.js-menu-buttonをクリックしたらp-sidebarにis-openクラスが付与される*/
+  /*.js-menu-buttonをクリックしたらl-containerにis-openクラスが付与される*/
+  /*.js-menu-buttonをクリックしたらbodyが固定される*/
   $(".js-menu-button").on("click", function () {
-    $(".p-sidebar ").toggleClass("is-open p-sidebar_move");
-    $(".l-container").toggleClass("is-open cover");
+    $(".p-sidebar ").toggleClass("is-open");
+    $(".l-container").toggleClass("is-open");
     $("body").toggleClass("fixed");
   });
 
+    /*js-menu-close-buttonをクリックしたらis-openクラスを削除 bodyに付与されていたfixedを削除*/
   $(".js-menu-close-button").on("click", function () {
-    //openクラスの要素を削除
-    $(".p-sidebar").removeClass("is-open p-sidebar_move");
-    $(".l-container").removeClass("is-open cover");
+    $(".p-sidebar").removeClass("is-open");
+    $(".l-container").removeClass("is-open");
     $("body").toggleClass("fixed");
   });
 });
